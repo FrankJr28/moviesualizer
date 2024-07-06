43f109e3ecc39f4f1177dc185e647aaec87bdd09
FrankJr28 <francisco.vasquezjr@alumnos.udg.mx>
2024-07-05 23:34:35 -0500

    first iteration declared

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,39 @@ import './App.css'
 
 import { useMovies } from './hooks/useMovies.js'
 import { Movies } from './components/Movies.jsx'
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+
+function useSearch(){
+  const [search, updateSearch] = useState('')
+  const [error, setError] = useState(null)
+  const isFirstInput = useRef(true)
+  ///////////////////////////
+  useEffect(()=>{
+    if(isFirstInput.current){
+      isFirstInput.current = search === ''
+      return
+    }
+    if(search === ''){
+      setError('No se puede buscar una pelicula vacia')
+      return
+    }
+    if(search.match(/^\d+$/)){
+      setError("No se puede buscar una pelicula con un nunmero")
+      return
+    }
+    if(search.length < 3){
+      setError("La busqueda debe al menos contener 3 caracteres")
+      return
+    }
+    setError(null)
+  }, [search])
+  ///////////////////////////
+  return { search, updateSearch, error }
+}
 
 function App() {
   const { movies } = useMovies();
-  const [query, setQuery] = useState('')
-  const [error, setError] = useState(null)
+  const { search, updateSearch, error } = useSearch()
 
   const handleSubmit = (event)=>{
     /*event.preventDefault()
@@ -17,34 +44,22 @@ function App() {
     console.log("hola");
     console.log(query.query)*/
     event.preventDefault()
-    console.log({query})
+    //console.log({query})
   }
 
   const handleChange = (event) =>{
-    setQuery(event.target.value)
+    //const newQuery = event.target.value
+    //if(newQuery.startswith('')) return
+    updateSearch(event.target.value)
   }
 
-  useEffect(()=>{
-    if(query === ''){
-      setError('No se puede buscar una pelicula vacia')
-      return
-    }
-    if(query.match(/^\d+$/)){
-      setError("No se puede buscar una pelicula con un nunmero")
-      return
-    }
-    if(query.length < 3){
-      setError("La busqueda debe al menos contener 3 caracteres")
-      return
-    }
-  }, [query])
-  
   return (
     <div className='page'>
       <header>
         <h2>Movie searcher</h2>
         <form className='form' onSubmit={handleSubmit}>
-          <input onChange={handleChange} name="query" placeholder='movie'/>
+          <input onChange={handleChange} style={{ border: '1px solid transparent', borderColor: error ? 'red' : 'transparent' }}
+          value={search} name="query" placeholder='movie'/>
           <button type='submit'>Search</button>
         </form>
         {error && <p className='error'>{error}</p>}
